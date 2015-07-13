d861ea37450f52cd07d5527245a3f59070e41845
z.w <charlenezell@gmail.com>
2015-07-13 18:00:24 +0800

    add readme.md , gulpfile refine debugMode ,add init.js to index.html

---
+++ readme.md
@@ -0,0 +1,45 @@
+# 安装方法
+~~xiaoming~~
+1. 安装node和npm
+[官网下载](https://nodejs.org/dist/v0.12.6/node-v0.12.6-x86.msi"点击进入")
+自带有npm和nodejs
+xiaoming
+
+function a(){console.log("hello")}
+~~~~~~~~~~~~html
+<table>
+<tr>
+<td>2</td>
+<td>4</td>
+<td>5</td>
+</tr>
+</table>
+2. 安装npm install -g zell-boot
+3. 安装cmd 执行 zell-boot
+
+1971. winter
+1972. spring
+#使用说明
+
+xiaoming
+===
+xiaoli
+xiaohong
+---
+
+<http://baidu.com>
+<a@yahoo.com>
+<@yahoo.com>
+I get 10 times more traffic from [Google] [1] than from
+[Yahoo] [2] or [MSN] [3].
+
+[1]:http://google.com/        "Google"
+[2]:http://search.yahoo.com/  "Yahoo Search"
+[3]:http://search.msn.com/    "MSN Search"
+
+xiaoming huishuo `print("hello haha")`
+
+A single backtick in a code span:`` ` ``
+
+A backtick-delimited string in a code span:`` `foo` ``
+
--- zell-boot/root/gulpfile.js
+++ zell-boot/root/gulpfile.js
@@ -1,4 +1,4 @@
-var isDebug=true;
+var isDebug = true;
 var gulp = require('gulp');
 var gulpif = require('gulp-if');
 var concat = require("gulp-concat");
@@ -19,31 +19,35 @@ var streamqueue = require('streamqueue');
 var del = require("del");
 var ld = require("lodash/");
 var fs = require("fs");
-var rev=require("gulp-rev");
+var rev = require("gulp-rev");
 var useref = require('gulp-useref');
 var replace = require("zell-grr");
-var os=require("os");
+var os = require("os");
 gulp.task("clean", function(cb) {
-    del("dest/",cb);
+    del("dest/", cb);
 });
 gulp.task("css", ["sprite"], function() {
-     var g=btnsHtmlArray.map(function(v){
-         return "<a class='actcommonbtns icon-"+v+"' href='###'></a>"+os.EOL;
+    var g = btnsHtmlArray.map(function(v) {
+        return "<a class='actcommonbtns icon-" + v + "' href='###'></a>" + os.EOL;
     }).join("");
-     fs.writeFileSync("btns.html",g);
-    return gulp.src("src/**/*.css")
-        .pipe(autoprefixer())
-        .pipe(cssmin({
-            compatibility: "ie6,ie7,ie8,+selectors.ie7Hack,+properties.iePrefixHack"
-        }))
-        .pipe(gulp.dest("dest/"));
+    fs.writeFileSync("btns.html", g);
+    var stream = gulp.src("src/**/*.css");
+    if (!isDebug) {
+        stream.pipe(autoprefixer())
+            .pipe(cssmin({
+                compatibility: "ie6,ie7,ie8,+selectors.ie7Hack,+properties.iePrefixHack"
+            }))
+    }
+    return stream.pipe(gulp.dest("dest/"));
 });
 gulp.task("js", ["clean"], function() {
-    return gulp.src("src/**/*.js")
+    var stream = gulp.src("src/**/*.js")
         .pipe(jshint())
         .pipe(jshint.reporter(stylish))
-        .pipe(uglify())
-        .pipe(gulp.dest("dest/"));
+    if (!isDebug) {
+        stream.pipe(uglify());
+    }
+    return stream.pipe(gulp.dest("dest/"));
 });
 gulp.task("html", ["clean", "js", "css"], function() {
     var assets1 = useref.assets({
@@ -54,13 +58,22 @@ gulp.task("html", ["clean", "js", "css"], function() {
     });
     return merge(
         gulp.src("src/wap/*.html").pipe(assets2).pipe(gulp.dest("dest/wap/"))
-        .pipe(gulp.dest("dest/wap/")).pipe(assets2.restore()).pipe(useref()).pipe(htmlmin({conditionals:true,quotes:false})).pipe(gulp.dest("dest/wap/")),
+        .pipe(gulp.dest("dest/wap/")).pipe(assets2.restore()).pipe(useref()).pipe(htmlmin({
+            conditionals: true,
+            quotes: false
+        })).pipe(gulp.dest("dest/wap/")),
         gulp.src("src/web/*.html").pipe(assets1).pipe(gulp.dest("dest/web/"))
-        .pipe(gulp.dest("dest/web/")).pipe(assets1.restore()).pipe(useref()).pipe(htmlmin({conditionals:true,quotes:false})).pipe(gulp.dest("dest/web/")),
-        gulp.src("src/*.html").pipe(htmlmin({conditionals:true,quotes:false})).pipe(gulp.dest("dest/"))
+        .pipe(gulp.dest("dest/web/")).pipe(assets1.restore()).pipe(useref()).pipe(htmlmin({
+            conditionals: true,
+            quotes: false
+        })).pipe(gulp.dest("dest/web/")),
+        gulp.src("src/*.html").pipe(htmlmin({
+            conditionals: true,
+            quotes: false
+        })).pipe(gulp.dest("dest/"))
     )
 });
-var btnsHtmlArray=[];
+var btnsHtmlArray = [];
 gulp.task("sprite", ["clean"], function() {
     var subSites = ld.compact(glob.sync("src/*").map(function(v) {
         if (fs.statSync(v).isDirectory()) {
@@ -71,7 +84,7 @@ gulp.task("sprite", ["clean"], function() {
     }));
     var jobs = [];
     var cssBundles = {};
-    btnsHtmlArray=[];
+    btnsHtmlArray = [];
     subSites.forEach(function(v) {
         cssBundles[v] = [];
         var _map = glob.sync(v + "/**/img/sprite/*").map(function(a) {
@@ -81,9 +94,9 @@ gulp.task("sprite", ["clean"], function() {
                 return realName;
             }
 
-             function getParamList(_a) {
+            function getParamList(_a) {
                 /*p8,v,h,s  默认用_分割，潜规则是图片命名不能有_*/
-                var a=p.basename(_a);
+                var a = p.basename(_a);
                 var arg = a.split("_");
                 arg.pop();
                 return arg;
@@ -111,53 +124,67 @@ gulp.task("sprite", ["clean"], function() {
             };
             cssBundles[v].push(d.css);
             // console.log("hello",_basePath.join("/"))
-             return (ld.contains(getParamList(a), "p8") ? d.img.pipe(pngmin()) : d.img.pipe(imagemin({
-                optimizationLevel: isDebug?0:5
+            return (ld.contains(getParamList(a), "p8") ? d.img.pipe(pngmin()) : d.img.pipe(imagemin({
+                optimizationLevel: 0
             }))).pipe(gulp.dest("dest/" + _basePath.join("/")));
         });
         var imgJob = es.concat.apply(null, _map);
-        jobs.push(imgJob);
+        if (_map.length > 0) {
+            jobs.push(imgJob);
+        }
         // console.log(cssBundles[v])
         // console.log("whatisv->%s", v);
         var cssJob = streamqueue.apply(null, [{
             objectMode: true
         }].concat(cssBundles[v])).pipe(concat("sprite.css")).pipe(cssmin({
             compatibility: "ie6,ie7,ie8,+selectors.ie7Hack,+properties.iePrefixHack"
-        })).pipe(gulp.dest(p.resolve("dest/" + cssBundles[v].extraData.basePath, "../../")));
-        jobs.push(cssJob);
+        }));
+        if (cssBundles[v].extraData && cssBundles[v].extraData.basePath) {
+            cssJob.pipe(gulp.dest(p.resolve("dest/" + cssBundles[v].extraData.basePath, "../../")));
+            jobs.push(cssJob);
+        }
     });
-    return merge.apply(this, jobs);
+    if (jobs.length > 0) {
+        return merge.apply(this, jobs);
+    }
 });
 gulp.task("resource", ["clean"], function() {
-    return merge(gulp.src(["src/**/*.{mp3,jpg,png,swf,gif,mp4,flv}", "!src/**/img/sprite/**/*","!src/**/img/bg/**/*"]).pipe(gulp.dest("dest/")),
-        gulp.src("src/**/img/bg/**/*").pipe(imagemin({progressive:true,optimizationLevel: isDebug?0:5})).pipe(gulp.dest("dest/")));
+    var imgStream = gulp.src("src/**/img/bg/**/*");
+    if (!isDebug) {
+        imgStream.pipe(imagemin({
+            progressive: true,
+            optimizationLevel: 0
+        }));
+    }
+    return merge(gulp.src(["src/**/*.{mp3,jpg,png,swf,gif,mp4,flv}", "!src/**/img/sprite/**/*", "!src/**/img/bg/**/*"]).pipe(gulp.dest("dest/")),
+        imgStream.pipe(gulp.dest("dest/")));
 });
 
 gulp.task("build", ["clean", "css", "js", "html", "resource", "sprite"]);
 // gulp.task("build", ["clean", "js", "html", "resource"]);
-gulp.task("makerevfile",["build"],function(){
+gulp.task("makerevfile", ["build"], function() {
     /*make rev configFile*/
     return gulp.src("dest/**/*.{html,css,js,png,jpg,swf,mp4,mp3,flv}").pipe(rev()).pipe(rev.manifest()).pipe(gulp.dest("./"))
-    // return gulp.src("dest/**/*.html").pipe()
+        // return gulp.src("dest/**/*.html").pipe()
 });
-gulp.task("rev",["makerevfile"],function(){
+gulp.task("rev", ["makerevfile"], function() {
     var mani = gulp.src("rev-manifest.json");
-     return gulp.src("dest/**/*.{html,css,js}")
-         .pipe(replace({
-             manifest: mani,
-             modifyUnreved:function(name,base){
-                 var a=p.relative(p.dirname(base),p.resolve("dest",name));
-                 a=a.replace(/\\/g,"/");
-                 return a;
-             },
-             modifyReved:function(name,unrevName,base){
-                 var rev=p.basename(name).split("-").pop().split(".")[0];
-                 var a=p.relative(p.dirname(base),p.resolve("dest",unrevName))+"?"+rev;
-                 a=a.replace(/\\/g,"/");
-                 return a;
-             }
-         }))
-         .pipe(gulp.dest("dest/"));
+    return gulp.src("dest/**/*.{html,css,js}")
+        .pipe(replace({
+            manifest: mani,
+            modifyUnreved: function(name, base) {
+                var a = p.relative(p.dirname(base), p.resolve("dest", name));
+                a = a.replace(/\\/g, "/");
+                return a;
+            },
+            modifyReved: function(name, unrevName, base) {
+                var rev = p.basename(name).split("-").pop().split(".")[0];
+                var a = p.relative(p.dirname(base), p.resolve("dest", unrevName)) + "?" + rev;
+                a = a.replace(/\\/g, "/");
+                return a;
+            }
+        }))
+        .pipe(gulp.dest("dest/"));
 });
 gulp.task("deploy", ["rev"], function() {
     return gulp.src(["dest/**/*"]).pipe(gulp.dest("<%= deployRootPath%><%= siteName%>/<%= activityPath%><%=sitePath%>")).pipe(gulp.dest("./"));
@@ -169,9 +196,3 @@ gulp.task('watch', function() {
 });
 
 gulp.task("default", ["watch", "deploy"]);
-
-
-
-
-
-
