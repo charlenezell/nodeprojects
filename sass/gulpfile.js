272eb0ba275938313e9c9a71e840bb6d2ee4c067
z.w <charlenezell@gmail.com>
2015-08-06 22:49:54 +0800

    init sass

---
+++ sass/gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var livereload = require('gulp-livereload');
+gulp.task('sass', function () {
+  return sass('./src/',{sourcemap: true})
+    .on('error', function (err) {
+        console.error('Error!', err.message);
+    })
+    .pipe(sourcemaps.write("./map/"))
+    .pipe(gulp.dest('./dest/'))
+    .pipe(livereload());
+});
+
+gulp.task('sass:watch', function () {
+    livereload.listen();
+  return gulp.watch('./**/*.scss', ['sass']);
+});
+gulp.task("default",["sass","sass:watch"]);
